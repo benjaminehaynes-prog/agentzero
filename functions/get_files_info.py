a78fbdebc3c0c3cddcc32e63c946f4d88ba09f94
benjaminehaynes-prog <benjaminhaynes@3YV2J64.localdomain>
2026-05-21 13:25:29 -0400

    File path script

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,7 +8,15 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir): 
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
-        
+        files = os.listdir(target_dir)
+        reviewed = []
+        for filename in files:
+            full_path = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(full_path)
+            file_size = os.path.getsize(full_path)
+            line = f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            reviewed.append(line)
+        result = "\n".join(reviewed)
+        return result
     except Exception as e:
         return f'Error: {e}'
